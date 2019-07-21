cbd892e490c50b3a77d59d6f00b2d73a3e41e27c
mao12312 <42489486+mao12312@users.noreply.github.com>
2019-07-22 00:26:35 +0900

    Tweak

--- More-types/p24/function-values.go
+++ More-types/p24/function-values.go
@@ -5,16 +5,22 @@ import (
 	"math"
 )
 
-func compute(fn func(float64,float64)float64)float64{
+func compute(fn func(float64, float64) float64) float64 {
 	return fn(3, 4)
 }
 
-func Main(){
-	hypot := func(x,y float64)float64{
-		return math.Sqrt(x*x+y*y)
+func Main() {
+	hypot := func(x, y float64) float64 {
+		return math.Sqrt(x*x + y*y)
 	}
-	fmt.Println(hypot(5,12))
+	test := func(a, b float64) float64 {
+		ans := a * b
+		return ans
+	}
+
+	fmt.Println(hypot(5, 12))
 
 	fmt.Println(compute(hypot))
 	fmt.Println(compute(math.Pow))
+	fmt.Println(compute(test))
 }
